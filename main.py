322e793ed43f79f088cb0e5b91c4b72b658dfc31
Julian Engel <julian13engel@gmail.com>
2015-09-29 22:32:49 +0300

    added some code from original source and cleaned it up

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import csv
 from PIL import Image, ImageDraw,ImageFont
 
 
-
-# Main image from base.jpg
-im = Image.open('base.jpg').convert('RGBA')
-W, H = im.size
-
-MaxSize = 200
-maxFontW = W * .90
-
 # Open the Local Hack Day CSV
 with open('data.csv', 'rb') as attendance:
 	#Set the reader
     reader = csv.reader(attendance)
+    
     #Iterate through
     for row in reader:
+
+		# Main image from base.jpg
+		im = Image.open('base.jpg').convert('RGBA')
+		W, H = im.size
+
+		MaxSize = 200
+		maxFontW = W * .90
+
+		nameSize = MaxSize
+        congrats = MaxSize/2
 
     	#Grab name and surname
     	first_name = row[0].upper()
@@ -23,6 +26,24 @@ with open('data.csv', 'rb') as attendance:
     	full_name = first_name + " " + last_name
 
     	# Set font and size
-        NameFont = ImageFont.truetype('fonts/Outage.ttf', venueSize)
-        address = ImageFont.truetype('fonts/Lato.ttf', addressSize)
-        
+        venue = ImageFont.truetype('fonts/Outage.ttf', nameSize)
+        address = ImageFont.truetype('fonts/Lato.ttf', congrats)
+
+        draw = ImageDraw.Draw(im)
+
+        # Find size of text
+        wVenue, hVenue = draw.textsize(venueName,font=venue)
+
+        # Make size smaller until width is less than size of maxFontW
+        while (wVenue > maxFontW):
+            nameSize = nameSize - 10
+            venue = ImageFont.truetype('fonts/Outage.ttf', nameSize)
+            wVenue, hVenue = draw.textsize(venueName,font=venue)
+
+        wAddress, hAddress = draw.textsize(addressDetails,font=address)
+
+        while (wAddress > maxFontW):
+            congrats = congrats - 10
+            address = ImageFont.truetype('fonts/OpenSansRegular.ttf', congrats)
+            wAddress, hAddress = draw.textsize(addressDetails,font=address)
+
