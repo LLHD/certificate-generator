68c4acb7de20fce2404e5221fb9fe97e194ef0d0
Julian Engel <julian13engel@gmail.com>
2015-09-29 22:31:07 +0300

    fixed it. Forgot to put the () after upper

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import csv
+from PIL import Image, ImageDraw,ImageFont
+
 with open('data.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
-    	first_name = row[0]
-    	last_name = row[1]
-        print str(first_name).upper + " " + Strlast_name.upper
+    	first_name = row[0].upper()
+    	last_name = row[1].upper()
+        print first_name + " " + last_name
