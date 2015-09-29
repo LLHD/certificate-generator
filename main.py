3e9149709335aa6ac722f007ff6a97b5e41af044
Julian Engel <julian13engel@gmail.com>
2015-09-29 22:31:56 +0300

    finished getting the data

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ MaxSize = 200
 maxFontW = W * .90
 
 # Open the Local Hack Day CSV
-with open('data.csv', 'rb') as f:
-    reader = csv.reader(f)
+with open('data.csv', 'rb') as attendance:
+	#Set the reader
+    reader = csv.reader(attendance)
+    #Iterate through
     for row in reader:
 
     	#Grab name and surname
     	first_name = row[0].upper()
     	last_name = row[1].upper()
     	full_name = first_name + " " + last_name
-    	
+
+    	# Set font and size
+        NameFont = ImageFont.truetype('fonts/Outage.ttf', venueSize)
+        address = ImageFont.truetype('fonts/Lato.ttf', addressSize)
         
