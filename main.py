79c0fa54d728bc0948e72901524adfa8d7f2a2f5
Julian Engel <julian13engel@gmail.com>
2015-09-29 22:31:46 +0300

    created everything up until full name from csv

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import csv
 from PIL import Image, ImageDraw,ImageFont
 
+
+
+# Main image from base.jpg
+im = Image.open('base.jpg').convert('RGBA')
+W, H = im.size
+
+MaxSize = 200
+maxFontW = W * .90
+
+# Open the Local Hack Day CSV
 with open('data.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
+
+    	#Grab name and surname
     	first_name = row[0].upper()
     	last_name = row[1].upper()
-        print first_name + " " + last_name
+    	full_name = first_name + " " + last_name
+    	
+        
