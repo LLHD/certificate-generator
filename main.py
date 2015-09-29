f39f93669b243c26d49faf138c7652c2d49bcb0e
Julian Engel <julian13engel@gmail.com>
2015-09-29 22:29:35 +0300

    changed around the tests

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import csv
 with open('data.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
-        print row[]
+        print row[] + " " +row[2]
         print "-------------------"
