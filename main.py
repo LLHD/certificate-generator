2246c247d626ac2f1dad020cc7990772b1a934de
Julian Engel <julian13engel@gmail.com>
2015-09-29 22:30:29 +0300

    created the variables and change to .upper weird output

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import csv
 with open('data.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
-        print row[] + " " +row[2]
-        print "-------------------"
+    	first_name = row[0]
+    	last_name = row[1]
+        print str(first_name).upper + " " + Strlast_name.upper
